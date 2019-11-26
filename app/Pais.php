4bf278bd0bdd2de5709e1c4ca62c0692c4c564f4
Crox A. Sánchez R <desarrollo@glovocapital.com>
2019-11-26 00:52:55 -0500

    Modelo País

--- app/Pais.php
+++ app/Pais.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Pais extends Model
 {
-    //
+    protected $primaryKey = 'pais_id';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'pais_nombre'
+    ];
+
+    public function manyEmpresas(){
+        return $this->hasMany(Empresa::class);
+    }
 }
