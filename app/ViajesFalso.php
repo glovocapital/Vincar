84504b5fea4639a154ae79ec5ceea4313cb86c0f
Crox A. Sánchez R <desarrollo@glovocapital.com>
2019-11-26 17:54:30 -0500

    Modelo ViajeFalso

--- app/ViajesFalso.php
+++ app/ViajesFalso.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class ViajesFalso extends Model
 {
-    //
+    protected $primaryKey = 'viaje_falso_id';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    /* protected $fillable = [
+        'tour_guia', 'tour_fec_inicio', 'tour_fec_fin', 'tour_finalizado'
+    ]; */
+    
+    public function belongsToEmpresa (){
+        return $this->belongsTo(Empresa::class, 'empresa_id', 'empresa_id');
+    }
 }
