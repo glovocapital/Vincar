52c8cd5f8b2ee8ad78f6b1cd00fc57b7a3116968
Alain Diaz <jadcve@gmail.com>
2019-11-29 03:18:50 -0300

    sidebar asset

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -1,7 +1,7 @@
 <!-- Main Sidebar Container -->
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="home" class="brand-link">
+    <a href="{{ route('home') }}" class="brand-link">
       <img src="adminlte/dist/img/logo-128x128.png"
            alt="Logo Vincar"
            class="brand-image img-circle elevation-3"
@@ -15,6 +15,7 @@
       <!-- Sidebar user (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
+
           <img src="adminlte/dist/img/avatar5.png" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
