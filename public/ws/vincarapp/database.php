bc080b1b68b52106659ed9acd6c12fcc2ba2c32b
Crox A. Sánchez R <desarrollo@glovocapital.com>
2020-01-06 10:35:56 -0400

    Web Service de prueba

---
+++ public/ws/vincarapp/database.php
@@ -0,0 +1,10 @@
+<?php
+
+return array(
+    "driver"    =>"pgsql",
+    "host"      =>"localhost",
+    "user"      =>"vincar",
+    "pass"      =>"vincar01",
+    "database"  =>"vincar",
+    "charset"   =>"utf8"
+);
