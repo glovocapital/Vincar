0ca196a9f555c83bb33d99343bf834e870474ff7
david segura <59055181+davidglovocapital@users.noreply.github.com>
2020-01-10 18:50:45 -0400

    Corregido el plugin datatable

--- resources/views/layouts/partials/footer-scripts.blade.php
+++ resources/views/layouts/partials/footer-scripts.blade.php
@@ -1,1 +1,27 @@
 <script src="{{ asset('base/js/app.js') }}"></script>
+<script>
+    $(function () {
+
+        if ("#dataTableAusentismo") {
+            $(".card-tools").hide();
+
+            var datatablesButtons = $("#dataTableAusentismo").DataTable({
+                responsive: true,
+                lengthChange: !1,
+                @if(Session::get('lang')=="es")
+                language: {
+                "url": "//cdn.datatables.net/plug-ins/1.10.15/i18n/Spanish.json"
+                 },
+                @endif
+                buttons: ["copy", "print"],
+
+            });
+
+            datatablesButtons.buttons().container().appendTo("#datatables-buttons_wrapper .col-md-6:eq(0)");
+
+        }
+
+    });
+
+
+</script>
