2e04992ec94a46b80cb6cb0854a860eb02b22c74
david segura <59055181+davidglovocapital@users.noreply.github.com>
2020-01-10 16:24:32 -0400

    Adaptando la plantilla

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,32 +1,33 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-   @include('layouts.partials.head')
+    @include('layouts.partials.head')
 </head>
-<body class="hold-transition sidebar-mini layout-fixed">
-<!-- Site wrapper -->
+
+<body>
 <div class="wrapper">
+
     @include('layouts.partials.nav')
-    @include('layouts.partials.sidebar')
-    <!-- Content Wrapper. Contains page content -->
-    <div class="content-wrapper">
-        @include('layouts.partials.header')
-        <!-- Main content -->
-        <section class="content">
-            <div class="container-fluid">
-                <div class="row">
-                    @yield('content')
-                </div>
-            </div>
-        </section>
-        <!-- /.content -->
+
+    <div class="main">
+        @include('layouts.partials.sidebar')
+
+        <main class="content">
+            @yield('content')
+        </main>
+
+        @include('layouts.partials.footer')
+
     </div>
-    <!-- /.content-wrapper -->
 </div>
-@include('layouts.partials.footer')
-@include('layouts.partials.control-sidebar')
+
+
+
 @include('layouts.partials.footer-scripts')
-@yield('sidebar-scripts')
+
 @yield('local-scripts')
+
 </body>
+
+
 </html>
--- resources/views/layouts/partials/footer-scripts.blade.php
+++ resources/views/layouts/partials/footer-scripts.blade.php
@@ -1,17 +1,1 @@
-<!-- jQuery -->
-
-<script src="{{ asset('adminlte/plugins/jquery/jquery.min.js') }}"></script>
-
-<!-- Bootstrap 4 -->
-
-<script src="{{ asset('adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-
-<!-- overlayScrollbars -->
-
-<script src="{{ asset('adminlte/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js') }}"></script>
-
-<!-- AdminLTE App -->
-<script src="{{ asset('adminlte/dist/js/adminlte.min.js') }}"></script>
-
-<!-- AdminLTE for demo purposes -->
-<script src="{{ asset('adminlte/dist/js/demo.js') }}"></script>
+<script src="{{ asset('base/js/app.js') }}"></script>
