85b3cab2a11215db19e88d112dd97398645d819b
Alain Diaz <jadcve@gmail.com>
2020-01-14 11:59:59 -0300

    cambios de estado vins

--- resources/views/vin/index.blade.php
+++ resources/views/vin/index.blade.php
@@ -192,6 +192,25 @@
                 </div>
             </div>
     </div>
+    <a href="#" data-toggle="modal" data-target="#miModal">
+        Abrir modal
+      </a>
+
+    <div class="modal fade" id="miModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                    <h4 class="modal-title" id="myModalLabel">Esto es un modal</h4>
+                </div>
+                <div class="modal-body">
+                    Texto del modal
+                </div>
+            </div>
+        </div>
+    </div>
 
 
 
