13e9987bac0c4773e4a96c466d96f9ef6e43156d
david segura <59055181+davidglovocapital@users.noreply.github.com>
2020-01-15 10:22:07 -0400

    Palabras para vins patio

---
+++ resources/lang/es/vins_patio.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: JEAN SERRANO
+ * Date: 11/01/2020
+ * Time: 12:41
+ */
+
+
+return [
+    "Porc_vehiculo"=>"% de vehiculos en el Patio",
+    "Mis_Patios"=>"Mis Patios",
+    "Capacidad"=>"Capacidad",
+    "Vehiculos_30dias"=>"Vehiculos con más de 30 Dias",
+    "Vehiculos_danos"=>"Vehiculos con Daños",
+    "Patio"=>"Patio",
+    "Bloque"=>"Bloque",
+    "Buscar_Bloque"=>"Buscar Bloque",
+    "Fila"=>"Fila",
+    "Seleccione"=>"Seleccione",
+    "Vaciar_Bloque"=>"Vaciar Bloque",
+    "U_Disponible"=>"Utilizados %d de %d [Disponibles: %d]",
+    "Salida"=>"Salida",
+    "Patio_Flotante"=>"Patio FLotante",
+    "Capacidad_Total"=>"Capacidad Total",
+    "Espacios_Disponibles"=>"Espacio Disponible"
+
+
+
+];
