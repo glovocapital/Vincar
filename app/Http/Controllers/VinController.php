1be1388a44a6cb6648ab5a0a4264112cd920b28e
Alain Diaz <jadcve@gmail.com>
2020-02-17 15:48:43 -0300

    Actualizacion vincontroller

--- app/Http/Controllers/VinController.php
+++ app/Http/Controllers/VinController.php
@@ -867,16 +867,19 @@ public function index2(Request $request)
         $vin_id =  Crypt::decrypt($id);
         $vin = Vin::findOrfail($vin_id);
 
-        $guia = $vin
+        $guia = DB::table('vins')
             ->join('guias_vins','guias_vins.vin_id','vins.vin_id')
             ->join('guias','guias_vins.guia_id','guias.guia_id')
             ->select('guias.guia_ruta')
-            ->first();
+            ->where('guias_vins.vin_id', $vin_id)
+            ->get();
 
-        if(!is_null($guia->guia_ruta))
+            //dd(empty($guia[0]->guia_ruta));
+
+        if(!empty($guia[0]->guia_ruta))
         {
 
-            return Storage::download("$guia->guia_ruta");
+            return Storage::download($guia[0]->guia_ruta);
 
         }else{
             flash('El vin no tiene guia asociada.')->error();
