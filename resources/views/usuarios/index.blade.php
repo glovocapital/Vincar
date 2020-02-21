1f84e8601bb4eef6d8cb7fa273cdcc1355de6d44
David Segura <asthar2010@gmail.com>
2020-02-21 05:27:34 -0400

    Rut validador

--- resources/views/usuarios/index.blade.php
+++ resources/views/usuarios/index.blade.php
@@ -2,147 +2,199 @@
 @section('title','Usuarios index')
 @section('content')
 
-<div class="col-lg-12">
-    <div class="ibox float-e-margins">
-        <div class="card card-default">
-            <div class="card-header">
-                <h3 class="card-title">Nuevo Usuario</h3>
-                <div class="card-tools">
-                    <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i></button>
-                    <button type="button" class="btn btn-tool" data-card-widget="remove"><i class="fas fa-remove"></i></button>
+    <div class="col-lg-12">
+        <div class="ibox float-e-margins">
+            <div class="card card-default">
+                <div class="card-header">
+                    <h3 class="card-title">Nuevo Usuario</h3>
+                    <div class="card-tools">
+                        <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i></button>
+                        <button type="button" class="btn btn-tool" data-card-widget="remove"><i class="fas fa-remove"></i></button>
+                    </div>
                 </div>
-            </div>
-            <div class="card-body">
-                <div class="row">
-                    <div class="col-md-4">
-                        {!! Form::open(['route'=> 'usuarios.store', 'method'=>'POST']) !!}
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-md-4">
+                            {!! Form::open(['route'=> 'usuarios.store', 'method'=>'POST']) !!}
 
-                        <div class="form-group">
-                            <label for="user_rut" >Rut <strong>*</strong></label>
-                            {!! Form::text('user_rut', null, ['placeholder'=>'Rut del usuario', 'class'=>'form-control col-sm-9', 'required']) !!}
+
+                                <label for="user_rut" >Rut <strong>*</strong></label>
+
+                            <div class="input-group" >
+
+                                {!! Form::text('user_rut', null, ['placeholder'=>'Rut del usuario', 'class'=>'form-control col-sm-9 rut', 'required']) !!}
+
+
+                                <div class="input-group-append">
+                                    <span class="input-group-text" id="validador">
+                                        <span style="color:red;" aria-hidden="true">&times;</span>
+                                    </span>
+
+                                </div>
+                            </div>
+
+
+                            <div class="form-group">
+                                <label for="user_nombre" >Nombre <strong>*</strong></label>
+                                {!! Form::text('user_nombre', null, ['placeholder'=>'Nombre del usuario', 'class'=>'form-control col-sm-9', 'required']) !!}
+                            </div>
+
+                            <div class="form-group">
+                                <label for="user_nombre" >Apellido del usuario <strong>*</strong></label>
+                                {!! Form::text('user_apellido', null, ['placeholder'=>'Apellido del usuario', 'class'=>'form-control col-sm-9', 'required']) !!}
+                            </div>
+
+                            <div class="form-group">
+                                <label for="user_telefono" >Teléfono <strong>*</strong></label>
+                                {!! Form::text('user_telefono', null, ['placeholder'=>'Telefono', 'class'=>'form-control col-sm-9']) !!}
+                            </div>
                         </div>
 
-                        <div class="form-group">
-                            <label for="user_nombre" >Nombre <strong>*</strong></label>
-                            {!! Form::text('user_nombre', null, ['placeholder'=>'Nombre del usuario', 'class'=>'form-control col-sm-9', 'required']) !!}
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                <label for="user_email" >Email <strong>*</strong></label>
+                                {!! Form::text('user_email', old('email'), ['class'=>'form-control col-sm-9', 'placeholder'=>'Email']) !!}
+                            </div>
+
+                            <div class="form-group">
+                                <label for="empresa_id" >Empresa <strong>*</strong></label>
+                                {!! Form::select('empresa_id', $empresa, null,['placeholder'=>'Seleccionar Empresa', 'class'=>'form-control col-sm-9', 'required'=>'required']) !!}
+                            </div>
+
+                            <div class="form-group">
+                                <label for="user_cargo" >Cargo del usuario <strong>*</strong></label>
+                                {!! Form::text('user_cargo', null, ['placeholder'=>'Cargo del usuario', 'class'=>'form-control col-sm-9', 'required']) !!}
+                            </div>
                         </div>
 
-                        <div class="form-group">
-                            <label for="user_nombre" >Apellido del usuario <strong>*</strong></label>
-                            {!! Form::text('user_apellido', null, ['placeholder'=>'Apellido del usuario', 'class'=>'form-control col-sm-9', 'required']) !!}
-                        </div>
-
-                        <div class="form-group">
-                            <label for="user_telefono" >Teléfono <strong>*</strong></label>
-                            {!! Form::text('user_telefono', null, ['placeholder'=>'Telefono', 'class'=>'form-control col-sm-9']) !!}
-                        </div>
-                    </div>
-
-                    <div class="col-md-4">
-                        <div class="form-group">
-                            <label for="user_email" >Email <strong>*</strong></label>
-                            {!! Form::text('user_email', old('email'), ['class'=>'form-control col-sm-9', 'placeholder'=>'Email']) !!}
-                        </div>
-
-                        <div class="form-group">
-                            <label for="empresa_id" >Empresa <strong>*</strong></label>
-                            {!! Form::select('empresa_id', $empresa, null,['placeholder'=>'Seleccionar Empresa', 'class'=>'form-control col-sm-9', 'required'=>'required']) !!}
-                        </div>
-
-                        <div class="form-group">
-                            <label for="user_cargo" >Cargo del usuario <strong>*</strong></label>
-                            {!! Form::text('user_cargo', null, ['placeholder'=>'Cargo del usuario', 'class'=>'form-control col-sm-9', 'required']) !!}
-                        </div>
-                    </div>
-
-                    <div class="col-md-4">
-                        <div class="form-group">
-                            <label for="user_pass" >Contraseña <strong>*</strong></label>
-                            {{ Form::password('user_pass',array('placeholder'=>'Contraseña','class' => 'form-control col-sm-9', 'required')) }}
-                            @if ($errors->has('user_pass'))
-                                <span class="invalid-feedback" role="alert">
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                <label for="user_pass" >Contraseña <strong>*</strong></label>
+                                {{ Form::password('user_pass',array('placeholder'=>'Contraseña','class' => 'form-control col-sm-9', 'required')) }}
+                                @if ($errors->has('user_pass'))
+                                    <span class="invalid-feedback" role="alert">
                                     <script>{{ $errors->first('user_pass') }}</script>
                                 </span>
-                            @endif
-                        </div>
+                                @endif
+                            </div>
 
-                        <div class="form-group">
-                            <label for="user_pass_rep" >Repita Contraseña <strong>*</strong></label>
-                            {{ Form::password('user_pass_confirmation',array('placeholder'=>'Repita la contraseña','class' => 'form-control col-sm-9', 'required')) }}
-                        </div>
+                            <div class="form-group">
+                                <label for="user_pass_rep" >Repita Contraseña <strong>*</strong></label>
+                                {{ Form::password('user_pass_confirmation',array('placeholder'=>'Repita la contraseña','class' => 'form-control col-sm-9', 'required')) }}
+                            </div>
 
-                        <div class="form-group">
-                            <label for="rol_id" >Rol <strong>*</strong></label>
-                            {!! Form::select('rol_id', $roles, null,['placeholder'=>'Seleccionar Rol', 'class'=>'form-control col-sm-9', 'required'=>'required']) !!}
-                        </div>                            
+                            <div class="form-group">
+                                <label for="rol_id" >Rol <strong>*</strong></label>
+                                {!! Form::select('rol_id', $roles, null,['placeholder'=>'Seleccionar Rol', 'class'=>'form-control col-sm-9', 'required'=>'required']) !!}
+                            </div>
+                        </div>
                     </div>
-                </div>
 
-                <div class="text-right pb-5">
-                    {!! Form::submit('Registrar usuario', ['class' => 'btn btn-primary block full-width m-b']) !!}
-                    {!! Form::close() !!}
-                </div>
+                    <div class="text-right pb-5">
+                        {!! Form::submit('Registrar usuario', ['class' => 'btn btn-primary block full-width m-b']) !!}
+                        {!! Form::close() !!}
+                    </div>
 
-                <div class="text-center texto-leyenda">
-                    <p><strong>*</strong> Campos obligatorios</p>
+                    <div class="text-center texto-leyenda">
+                        <p><strong>*</strong> Campos obligatorios</p>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 
 
-<div class="col-lg-12">
-    <div class="ibox float-e-margins">
-        <div class="card card-default">
-            <div class="card-header">
-                <h3 class="card-title">Listado de empresas</h3>
-                <div class="card-tools">
-                    <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i></button>
-                    <button type="button" class="btn btn-tool" data-card-widget="remove"><i class="fas fa-remove"></i></button>
+    <div class="col-lg-12">
+        <div class="ibox float-e-margins">
+            <div class="card card-default">
+                <div class="card-header">
+                    <h3 class="card-title">Listado de empresas</h3>
+                    <div class="card-tools">
+                        <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i></button>
+                        <button type="button" class="btn btn-tool" data-card-widget="remove"><i class="fas fa-remove"></i></button>
+                    </div>
                 </div>
-            </div>
-            <div class="card-body">
+                <div class="card-body">
                 <!--   <div class="col-lg-12 pb-3 pt-2">
                             <a href="{{ route('usuarios.create') }}" class = 'btn btn-primary'>Crear nuevo Usuario</a>
                         </div>
                 -->
-                <div class="table-responsive">
-                    <table class="table table-hover" id="dataTableAusentismo" width="100%" cellspacing="0">
-                        <thead>
+                    <div class="table-responsive">
+                        <table class="table table-hover" id="dataTableAusentismo" width="100%" cellspacing="0">
+                            <thead>
                             <tr>
                                 <th>Nombres</th>
                                 <th>Correo</th>
                                 <th>Rol</th>
                                 <th>Empresa</th>
                                 <th>Acci&oacute;n</th>
-                            <!-- <th>Desactivar</th>  -->
+                                <!-- <th>Desactivar</th>  -->
                             </tr>
-                        </thead>
-                        <tbody>
-                        @foreach($usuarios as $us)
-                            <tr>
-                                <td><small>{{ $us->user_nombre . ' ' . $us->user_apellido }}</small></td>
-                                <td><small>{{ $us->email }}</small></td>
-                                <td><small>{{ $us->oneRol->rol_desc }}</small></td>
-                                <td><small>{{ $us->belongsToEmpresa->empresa_razon_social }}</small></td>
-                                <td>
-                                    <small>
-                                        <a href="{{ route('usuarios.edit',  Crypt::encrypt($us->user_id)) }}" class="btn-empresa"><i class="far fa-edit"></i></a>
-                                    </small>
-                                    <small>
+                            </thead>
+                            <tbody>
+                            @foreach($usuarios as $us)
+                                <tr>
+                                    <td><small>{{ $us->user_nombre . ' ' . $us->user_apellido }}</small></td>
+                                    <td><small>{{ $us->email }}</small></td>
+                                    <td><small>{{ $us->oneRol->rol_desc }}</small></td>
+                                    <td><small>{{ $us->belongsToEmpresa->empresa_razon_social }}</small></td>
+                                    <td>
+                                        <small>
+                                            <a href="{{ route('usuarios.edit',  Crypt::encrypt($us->user_id)) }}" class="btn-empresa"><i class="far fa-edit"></i></a>
+                                        </small>
+                                        <small>
                                             <a href = "{{ route('usuarios.destroy', Crypt::encrypt($us->user_id))  }}" onclick="return confirm('¿Esta seguro que desea eliminar este elemento?')" class="btn-empresa"><i class="far fa-trash-alt"></i>
                                             </a>
-                                    </small>
-                                </td>
-                            </tr>
+                                        </small>
+                                    </td>
+                                </tr>
 
-                        @endforeach
-                        </tbody>
-                    </table>
+                            @endforeach
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
-@stop
+@endsection
+
+@section('local-scripts')
+    <script>
+        $(function(){
+
+            $('.rut').keyup(function(){
+
+                $("#validador").html('<span style="color:red;" aria-hidden="true">&times;</span>');
+
+
+                var Ts = $(this).val().split("-");
+                var T = Ts[0];
+
+
+                var M=0,S=1;
+                for(;T;T=Math.floor(T/10))
+                    S=(S+T%10*(9-M++%6))%11;
+                //return S?S-1:'k';
+
+                if(Ts[0].length==7 || Ts[0].length==8){
+
+                   if(Ts.length ==2){
+                       if(S-1==Ts[1]){
+                           $("#validador").html('<i style="color:green"  class="fa fa-check"></i>');
+                       }
+                   }
+
+                }
+
+
+
+
+
+
+            });
+
+        });
+
+        </script>
+@endsection
