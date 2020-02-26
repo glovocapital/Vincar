7f6f0193339450192c8ea0068d127d6c644b90d6
Crox A. Sánchez R <desarrollo@glovocapital.com>
2020-02-25 23:35:10 -0400

    Modificación de CampaniaController y vista principal para que muestre campañas según el rol del usuario

--- resources/views/campania/solicitudCampania.blade.php
+++ resources/views/campania/solicitudCampania.blade.php
@@ -45,8 +45,6 @@
 
                         {!! Form::submit('Buscar vin ', ['class' => 'btn btn-primary block full-width m-b', 'id'=>'btn-src']) !!}
 
-                        <a href="{{ route('campania.index') }}" class = 'btn btn-success'>Ver Campañas</a>
-
                         {!! Form::close() !!}
 
                     </div>
@@ -175,6 +173,65 @@
 @include('campania.partials.modal_solicitud_campania')
 @include('campania.partials.modal_solicitar_campania_lotes')
 
+<div class="row">
+    <div class="col-lg-12">
+        <div class="ibox float-e-margins">
+            <div class="card card-default">
+                <div class="card-header">
+                    <h3 class="card-title">Listado de Campañas</strong></h3>
+                    <div class="card-tools">
+                        <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i></button>
+                        <button type="button" class="btn btn-tool" data-card-widget="remove"><i class="fas fa-remove"></i></button>
+                    </div>
+                </div>
+                
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table table-hover" id="dataTableCampanias" width="100%" cellspacing="0">
+                            <thead>
+                                <tr>
+                                    <th>Código VIN</th>
+                                    <th>Descripción Campañas</th>
+                                    <th>Fecha Finalización</th>
+                                    <th>Observaciones</th>
+                                    <th>Usuario Solicitante</th>
+                                    <th>Acciones</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                            @foreach($campanias as $campania)
+                            <tr>
+                                <td><small>{{ $campania->codigoVin() }}</small></td>
+                                <td><small>
+                                @foreach($arrayTCampanias as $tipoCamp)
+                                    @foreach($tipoCamp as $tCamp)
+                                        @if($campania->campania_id === $tCamp->campania_id)   
+                                            <button class="btn btn-xs btn-info">{{ $tCamp->tipo_campania_descripcion }}</button>
+                                        @endif
+                                    @endforeach
+                                @endforeach
+                                </small></td>
+                                <td><small>{{ $campania->campania_fecha_finalizacion }}</small></td>
+                                <td><small>{{ $campania->campania_observaciones }}</small></td>
+                                <td><small>{{ $campania->nombreUsuario() }}</small></td>
+                                <td>
+                                    <small>
+                                        <a href="{{ route('campania.edit', Crypt::encrypt($campania->campania_id)) }}" class="btn-bloque" title="Editar Campania"><i class="far fa-edit"></i></a>
+                                    </small>
+                                    <small>
+                                        <a href = "{{ route('campania.destroy', Crypt::encrypt($campania->campania_id)) }}" onclick="return confirm('¿Esta seguro que desea eliminar este elemento?')" class="btn-bloque" title="Eliminar campaña"><i class="far fa-trash-alt"></i></a>
+                                    </small>
+                                </td>
+                            </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
 @stop
 @section('local-scripts')
