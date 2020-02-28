8f72dd5d683874098ce5083f40617e5b65673d90
David Segura <asthar2010@gmail.com>
2020-02-28 11:08:01 -0300

    Aplicado rut validador

--- resources/views/usuarios/edit.blade.php
+++ resources/views/usuarios/edit.blade.php
@@ -7,7 +7,7 @@
         <div class="ibox float-e-margins">
             <div class="card card-default">
                 <div class="card-header">
-                    <h3 class="card-title">Nuevo Usuario</h3>
+                    <h3 class="card-title">Editar Usuario</h3>
                     <div class="card-tools">
                         <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i></button>
                         <button type="button" class="btn btn-tool" data-card-widget="remove"><i class="fas fa-remove"></i></button>
@@ -18,9 +18,18 @@
                         <div class="col-md-4">
                             {!! Form::open(['route'=> ['usuarios.update', Crypt::encrypt($usuario->user_id)], 'method'=>'PATCH']) !!}
 
-                            <div class="form-group">
-                                <label for="user_rut" >Rut <strong>*</strong></label>
-                                {!! Form::text('user_rut', $usuario->user_rut, ['placeholder'=>'Rut del usuario', 'class'=>'form-control col-sm-9', 'required']) !!}
+                            <label for="user_rut" >Rut <strong>*</strong></label>
+
+                            <div class="input-group">
+
+                                {!! Form::text('user_rut', $usuario->user_rut, ['placeholder'=>'Rut del usuario', 'class'=>'form-control col-sm-9 rut', 'required']) !!}
+
+                                <div class="input-group-append">
+                                    <span class="input-group-text" id="validador">
+                                        <span style="color:red;" aria-hidden="true">&times;</span>
+                                    </span>
+
+                                </div>
                             </div>
 
                             <div class="form-group">
@@ -94,3 +103,44 @@
 
 @stop
 
+@section('local-scripts')
+    <script>
+        $(function(){
+
+            $('.rut').keyup(function(){
+
+                $("#validador").html('<span style="color:red;" aria-hidden="true">&times;</span>');
+
+
+                var Ts = $(this).val().split("-");
+                var T = Ts[0];
+
+
+                var M=0,S=1;
+                for(;T;T=Math.floor(T/10))
+                    S=(S+T%10*(9-M++%6))%11;
+                //return S?S-1:'k';
+
+                if(Ts[0].length==7 || Ts[0].length==8){
+
+                    if(Ts.length ==2){
+                        console.log(S-1);
+                        if(S-1==Ts[1]){
+                            $("#validador").html('<i style="color:green"  class="fa fa-check"></i>');
+                        }
+                    }
+
+                }
+
+
+            });
+
+            setTimeout(function(){
+                $('.rut').trigger("keyup");
+            },1000);
+
+
+        });
+
+    </script>
+@endsection
