c3d9466986ba2d5120472dc78d4a0fad1a2113b7
Crox A. Sánchez R <desarrollo@glovocapital.com>
2020-05-28 19:52:44 -0500

    Corrección de migración

--- database/migrations/2020_02_11_220848_guia_vins.php
+++ database/migrations/2020_02_11_220848_guia_vins.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class GuiasVins extends Migration
+class GuiaVins extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class GuiasVins extends Migration
      */
     public function up()
     {
-        Schema::create('guias_vins', function (Blueprint $table) {
+        Schema::create('guia_vins', function (Blueprint $table) {
             $table->bigIncrements('guia_vin_id');
 
             $table->unsignedBigInteger('vin_id');
@@ -34,6 +34,6 @@ class GuiasVins extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('guias_vins');
+        Schema::dropIfExists('guia_vins');
     }
 }
