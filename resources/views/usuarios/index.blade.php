8b106843f70b5c2cde8e15d05e3870c3c1fd3400
croxsanchez <crox.sanchez@gmail.com>
2021-09-01 12:43:24 -0500

    Corrección en vista de usuarios

--- resources/views/usuarios/index.blade.php
+++ resources/views/usuarios/index.blade.php
@@ -28,7 +28,7 @@
     <div class="ibox float-e-margins">
         <div class="card card-default">
             <div class="card-header">
-                <h3 class="card-title">Listado de empresas</h3>
+                <h3 class="card-title">Listado de Usuarios</h3>
                 <div class="card-tools">
                     <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i></button>
                     <button type="button" class="btn btn-tool" data-card-widget="remove"><i class="fas fa-remove"></i></button>
@@ -118,7 +118,7 @@
             $("#nuevoUsuario").modal('show');
         });
 
-        datatablesButtons = $('[id="dataTableUsuarios"]').DataTable({
+        $('#dataTableUsuarios').DataTable({
             searching: true,
             bSortClasses: false,
             deferRender:true,
