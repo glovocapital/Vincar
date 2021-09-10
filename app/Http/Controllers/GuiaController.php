9d62d7c7e11244f425730b718538222da02f5eef
croxsanchez <crox.sanchez@gmail.com>
2021-09-10 17:25:45 -0500

    Corrección de filtro por VIN en vista de Guías.

--- app/Http/Controllers/GuiaController.php
+++ app/Http/Controllers/GuiaController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Empresa;
 use App\Guia;
+use App\GuiaVin;
 use App\Http\Middleware\CheckSession;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
@@ -55,7 +56,11 @@ class GuiaController extends Controller
 
         // Filtro de búsqueda VIN
         if($request->vin_numero){
-            $query->where('vin_codigo', $request->vin_numero);
+            $vin_id = Vin::where('vin_codigo', $request->vin_numero)->value('vin_id');
+            $guia_vin = GuiaVin::where('vin_id', $vin_id)->first();
+            $guia = Guia::where('guia_id', $guia_vin->guia_id)->first();
+
+            $query->where('guia_id', $guia->guia_id);
         }
 
         // Filtro de búsqueda por empresa
